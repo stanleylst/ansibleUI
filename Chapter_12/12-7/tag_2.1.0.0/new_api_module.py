0cc9f7216d97ba3a5bb13abfb6e2ce17308c1a3b
deploy <targetoyes@163.com>
2016-07-16 06:51:02 -0700

    第12章 Ansible模块编写

--- Chapter_12/12-7/tag_2.1.0.0/new_api_module.py
+++ Chapter_12/12-7/tag_2.1.0.0/new_api_module.py
@@ -5,13 +5,13 @@ from ansible.vars import VariableManager
 from ansible.inventory import Inventory
 from ansible.playbook.play import Play
 from ansible.executor.task_queue_manager import TaskQueueManager
-Options = namedtuple('Options', ['listtags', 'listtasks', 'listhosts', 'syntax', 'connection','module_path', 'forks', 'remote_user', 'private_key_file', 'ssh_common_args', 'ssh_extra_args',
+Options = namedtuple('Options', ['listtags', 'listtasks', 'listhosts', 'syntax', 'connection','module_path', 'forks', 'private_key_file', 'ssh_common_args', 'ssh_extra_args',
 'sftp_extra_args', 'scp_extra_args', 'become', 'become_method', 'become_user', 'verbosity', 'check'])
 variable_manager = VariableManager()
 loader = DataLoader()
 
-options = Options(listtags=False, listtasks=False, listhosts=False, syntax=False, connection='ssh', module_path=None, forks=100, remote_user='deploy', private_key_file=None,
-ssh_common_args=None, ssh_extra_args=None, sftp_extra_args=None, scp_extra_args=None, become=True, become_method=None, become_user='deploy', verbosity=None, check=False)
+options = Options(listtags=False, listtasks=False, listhosts=False, syntax=False, connection='ssh', module_path=None, forks=100, private_key_file=None,
+ssh_common_args=None, ssh_extra_args=None, sftp_extra_args=None, scp_extra_args=None, become=False, become_method=None, become_user=None, verbosity=None, check=False)
 passwords = {}
 inventory = Inventory(loader=loader, variable_manager=variable_manager)
 variable_manager.set_inventory(inventory)
