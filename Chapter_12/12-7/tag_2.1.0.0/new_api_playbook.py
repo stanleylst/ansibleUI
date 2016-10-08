97d5616b2f3c5f7f8a43c8d33c3accf23b5afaee
Spoon.GJ <targetoyes123@gmail.com>
2016-10-08 11:28:33 +0800

    Update new_api_playbook.py

--- Chapter_12/12-7/tag_2.1.0.0/new_api_playbook.py
+++ Chapter_12/12-7/tag_2.1.0.0/new_api_playbook.py
@@ -8,7 +8,10 @@ from ansible.parsing.dataloader import DataLoader
 from ansible.vars import VariableManager
 from ansible.inventory import Inventory
 from ansible.executor.playbook_executor import PlaybookExecutor
-from ansible.utils.display import log_file
+try:
+    from ansible.utils.display import log_file
+except:
+    pass
 import sys
 
 variable_manager = VariableManager()
@@ -16,8 +19,8 @@ loader = DataLoader()
 
 inventory = Inventory(loader=loader, variable_manager=variable_manager,  host_list='/etc/ansible/hosts')
 
-Options = namedtuple('Options', ['listtags', 'listtasks', 'listhosts', 'syntax', 'connection','module_path', 'forks', 'private_key_file', 'ssh_common_args', 'ssh_extra_args', 'sftp_extra_args', 'scp_extra_args', 'become', 'become_method', 'become_user', 'verbosity', 'check'])
-options = Options(listtags=False, listtasks=False, listhosts=False, syntax=False, connection='ssh', module_path=None, forks=100,private_key_file=None, ssh_common_args=None, ssh_extra_args=None, sftp_extra_args=None, scp_extra_args=None, become=False, become_method=None, become_user=None, verbosity=None, check=False)
+Options = namedtuple('Options', ['listtags', 'listtasks', 'listhosts', 'syntax', 'connection','module_path', 'forks', 'private_key_file', 'ssh_common_args', 'ssh_extra_args', 'sftp_extra_args', 'scp_extra_args', 'become', 'become_method', 'become_user', 'remote_user', 'verbosity', 'check'])
+options = Options(listtags=False, listtasks=False, listhosts=False, syntax=False, connection='ssh', module_path=None, forks=100,private_key_file=None, ssh_common_args=None, ssh_extra_args=None, sftp_extra_args=None, scp_extra_args=None, become=False, become_method=None, become_user=None, remote_user=None, verbosity=None, check=False)
 
 playbook_path = 'test.yml'                    # modify here, change playbook
 variable_manager.extra_vars = {"args": "pong",} # modify here, This can accomodate various other command line arguments.`
