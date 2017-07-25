7292b606500785ae8f74f51c58299f2da1efe925
Spoon.GJ <targetoyes123@gmail.com>
2017-07-25 20:12:36 +0800

    第12章 Ansible模块编写
    第12章 Ansible模块编写

--- Chapter_12/12-7/tag_2.1.0.0/Ansi_play2.py
+++ Chapter_12/12-7/tag_2.1.0.0/Ansi_play2.py
@@ -10,37 +10,61 @@ from ansible.parsing.dataloader import DataLoader
 from ansible.vars import VariableManager
 from ansible.inventory import Inventory
 from ansible.executor.playbook_executor import PlaybookExecutor
-from ansible.utils.display import log_file, log_add
+try:
+    from ansible.utils.display import log_file, log_add
+except:
+    print('after modifying ansible.utils.display, you can use Ansi_play2.run()')
 from ansible.plugins.callback import CallbackBase
 from ansible.errors import AnsibleParserError
 
+from ansible.playbook.play import Play
+from ansible.executor.task_queue_manager import TaskQueueManager
+from copy import copy
+
 class ResultCallback(CallbackBase):
-    ''' if needed, you can modify it yourself'''
+    ''' if needed, you can modify it yourself'''    
     def __init__(self, *args):
         super(ResultCallback, self).__init__(*args)
-        self.ok = json.dumps({})
-        self.fail = json.dumps({})
-        self.unreachable = json.dumps({})
+        self.ok = {}
+        self.fail = {}
+        self.unreachable = {}
         self.playbook = ''
         self.no_hosts = False
+        self.msg_list = {}
+
+    def deal_result(self, res, host, status):
+        result = copy(res)
+        del_keys = [key for key in result.keys() if key.startswith('_')]
+        del_keys += ['invocation']
+        for key in del_keys:
+            del result[key]
+        if host not in self.msg_list:
+            self.msg_list[host] = {'msg': [], 'status': status}
+        if self.msg_list[host]['status'] in ['ok']:
+            self.msg_list[host]['status'] = status
+        self.msg_list[host]['msg'].append(result['stdout'] + result['stderr'])
+        return result
    
     def v2_runner_on_ok(self, result):
         host = result._host.get_name()
         self.runner_on_ok(host, result._result)
-        data = json.dumps({host: result._result}, indent=4)
-        self.ok = data
+        if host not in self.ok:
+            self.ok[host] = []
+        self.ok[host].append(self.deal_result(result._result, host, 'ok'))
 
     def v2_runner_on_failed(self, result, ignore_errors=False):
         host = result._host.get_name()
         self.runner_on_failed(host, result._result, ignore_errors)
-        data = json.dumps({host: result._result}, indent=4)
-        self.fail = data
+        if host not in self.fail:
+            self.fail[host] = []
+        self.fail[host].append(self.deal_result(result._result, host, 'failed'))
 
     def v2_runner_on_unreachable(self, result):
         host = result._host.get_name()
         self.runner_on_unreachable(host, result._result)
-        data = json.dumps({host: result._result}, indent=4)
-        self.unreachable = data
+        if host not in self.unreachable:
+            self.unreachable[host] = []
+        self.unreachable[host].append(self.deal_result(result._result, host, 'unreachable'))
 
     def v2_playbook_on_play_start(self, play):
         self.playbook_on_play_start(play.name)
@@ -63,6 +87,7 @@ class Ansi_Play2(object):
                         passwords={},
                         check=False):
         self.playbook = playbook
+        self.extra_vars = extra_vars
         self.passwords = passwords
         Options = namedtuple('Options',
                    ['listtags', 'listtasks', 'listhosts', 'syntax', 'connection','module_path',
@@ -80,7 +105,6 @@ class Ansi_Play2(object):
         if ansible_cfg != None:
             os.environ["ANSIBLE_CONFIG"] = ansible_cfg
         self.variable_manager = VariableManager()
-        self.variable_manager.extra_vars = extra_vars
         self.loader = DataLoader()
         self.inventory = Inventory(loader=self.loader, variable_manager=self.variable_manager,  host_list=host_list)
     
@@ -113,6 +137,34 @@ class Ansi_Play2(object):
         complex = '\n'.join(log_add)
         return code, results, complex
 
+    def play(self, hosts='localhost', module='setup'):
+        play_source =  dict(
+            name = "Ansible Play",
+            hosts = 'localhost',
+            gather_facts = 'no',
+            tasks = [
+                dict(action=dict(module='setup'))
+             ]
+        )
+        results_callback = ResultCallback()
+        play = Play().load(play_source, variable_manager=self.variable_manager, loader=self.loader)
+        qm = None
+        try:
+            tqm = TaskQueueManager(
+                  inventory=self.inventory,
+                  variable_manager=self.variable_manager,
+                  loader=self.loader,
+                  options=self.options,
+                  passwords=self.passwords,
+                  stdout_callback=results_callback,
+             )
+            result = tqm.run(play)
+            return results_callback
+        finally:
+            if tqm is not None:
+                tqm.cleanup()
+
+
     def run_need_data(self):
         if not os.path.exists(self.playbook):
             code = 1000
@@ -143,20 +195,26 @@ class Ansi_Play2(object):
                       'msg': 'no_hosts'}
             return code, simple, complex
         else:
-            ok = json.loads(results_callback.ok)
-            fail = json.loads(results_callback.fail)
-            unreachable = json.loads(results_callback.unreachable)
+            msg_list = results_callback.msg_list
+            ok = results_callback.ok
+            fail = results_callback.fail
+            unreachable = results_callback.unreachable
+            ok_all = list(ok.keys())
+            fail_all = list(fail.keys())
+            unreachable_hosts = list(unreachable.keys())
+            fail_hosts = list(set(fail_all) - set(unreachable_hosts))
+            ok_hosts = list(set(ok_all) - set(unreachable_hosts) - set(fail_hosts))
             if code != 0:
                 complex = {'playbook': results_callback.playbook, 'ok': ok,
                  'fail': fail, 'unreachable': unreachable, 'flag': False}
-                simple = {'executed': True, 'flag': False, 'playbook': self.playbook,
-                         'msg': {'playbook': self.playbook, 'ok_hosts': ok.keys(), 'fail': fail.keys(), 'unreachable': unreachable.keys()}}
+                simple = {'executed': True, 'flag': False, 'playbook': self.playbook, 'msg_list': msg_list,
+                         'msg': {'playbook': self.playbook, 'ok_hosts': ok_hosts, 'fail': fail_hosts, 'unreachable': unreachable_hosts}}
                 return code, simple, complex
             else:
                 complex = {'playbook': results_callback.playbook, 'ok': ok,
                  'fail': fail, 'unreachable': unreachable, 'flag': True}
-                simple = {'executed': True, 'flag': True, 'playbook': self.playbook,
-                          'msg': {'playbook': self.playbook, 'ok_hosts': ok.keys(), 'fail': fail.keys(), 'unreachable': unreachable.keys()}}
+                simple = {'executed': True, 'flag': True, 'playbook': self.playbook, 'msg_list': msg_list,
+                         'msg': {'playbook': self.playbook, 'ok_hosts': ok_hosts, 'fail': fail_hosts, 'unreachable': unreachable_hosts}}
                 return code, simple, complex
 
 if __name__ == '__main__':
@@ -165,11 +223,14 @@ if __name__ == '__main__':
     # if you need data of detail to combine with your program, choose first. 
     # if not care about detail and want to add to log, choose second.
     code, simple, complex = book2.run_need_data()   # get more details about playbook 
-    print code, simple, complex
+    print(code, simple, complex)
+    print(simple['msg_list'])
     # simple: {'msg': {'fail': [], 'unreachable': [], 'ok_hosts': [u'localhost'], 'playbook': 'test.yml'}, 'flag': True, 'executed': True, 'playbook': 'test.yml'} 
     # complex: {'code': 0, 'ok': {u'localhost': {u'invocation': {u'module_name': u'debug', u'module_args': {u'var': u'res'}}, u'res': {u'changed': True, u'end': u'2016-07-16 00:29:42.936217', u'stdout': u'123', u'cmd': u'bash -c "echo 123; sleep 10"', u'rc': 0, u'start': u'2016-07-16 00:29:32.932963', u'stderr': u'', u'delta': u'0:00:10.003254', u'stdout_lines': [u'123'], u'warnings': []}, u'changed': False, u'_ansible_verbose_always': True, u'_ansible_no_log': False}}, 'flag': True, 'playbook': u'test', 'fail': {}, 'unreachable': {}}
-    code, simple, complex = book2.run('/tmp/aa.log')   #  get simple result about playbook, and log detail in log_file
-    print code, simple, complex
+    #code, simple, complex = book2.run('/tmp/aa.log')   #  get simple result about playbook, and log detail in log_file
+    #print(code, simple, complex)
     # simple: 0
     # complex: [{u'localhost': {'unreachable': 0, 'skipped': 0, 'ok': 2, 'changed': 1, 'failures': 0}}]
     # log_file: '/tmp/aa.log'
+    #complex = book2.play()   #  get simple result about playbook, and log detail in log_file
+    #print(complex.ok, complex.fail)
